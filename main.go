1e164e10ec6fdb1b63d552ea587a67ec24af56d0
Zoltan Bodor <zbodor@utilitywarehouse.co.uk>
2018-08-11 15:41:36 +0100

    Add default domain name

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"golang.org/x/crypto/acme/autocert"
 )
 
-var domain = flag.String("domain", "", "Your domain name")
+var domain = flag.String("domain", "dragffy.ro", "Your domain name")
 
 func handler(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
