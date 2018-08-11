e62a314018c2a0acc1253b06ae40d5bb065a15ac
Zoltan Bodor <zbodor@utilitywarehouse.co.uk>
2018-08-11 15:04:23 +0100

    Init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Sample https hello-world repo
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+
+	"golang.org/x/crypto/acme/autocert"
+)
+
+var domain = flag.String("domain", "", "Your domain name")
+
+func handler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte("Hello / Welcome / Szia / Isten hozott / Bine ai venit!\n"))
+}
+
+func main() {
+	flag.Parse()
+	http.HandleFunc("/", handler)
+	log.Fatal(http.Serve(autocert.NewListener(*domain), nil))
+}
